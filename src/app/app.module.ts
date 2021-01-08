b20954d265076bd2f694c705ae1740b12527e903
Daniel Limas <amedlimas97@gmail.com>
2021-01-07 22:30:09 -0700

    Didn't finish app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { AuthModule, AuthService } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtectedComponent,
+    PricesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-g4qbhd2p.us.auth0.com',
+      clientId: 'pyzlFEmh5tRNUinETeQIoTRxTWMtDtIz',
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
